561461660dea9e73976aaa0a8d25034fff8b6bbe
mjkjkjk <mateusz.jan.kulesza@gmail.com>
2025-01-15 18:18:35 +0100

    rays

--- src/color.rs
+++ src/color.rs
@@ -1,5 +1,10 @@
-use std::fmt;
-
 use crate::vec3::Vec3;
-
+use std::io::Write;
 pub type Color = Vec3;
+
+pub fn write_color(file: &mut std::fs::File, pixel_color: Color) -> std::io::Result<()> {
+    let r = (255.999 * pixel_color.x()) as u8;
+    let g = (255.999 * pixel_color.y()) as u8;
+    let b = (255.999 * pixel_color.z()) as u8;
+    writeln!(file, "{} {} {}", r, g, b)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{command, Parser};
-use color::Color;
+use color::{write_color, Color};
 use point::Point3;
 use ray::Ray;
 use std::fs::File;
@@ -11,9 +11,6 @@ mod point;
 mod ray;
 mod vec3;
 
-const IMAGE_WIDTH: u32 = 256;
-const IMAGE_HEIGHT: u32 = 256;
-
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -21,31 +18,49 @@ struct Args {
     output_file: String,
 }
 
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction().unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
 fn main() -> std::io::Result<()> {
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    let image_width: u32 = 400;
+    let image_height: u32 = (image_width as f64 / aspect_ratio) as u32;
+
+    let focal_length: f64 = 1.0;
+    let viewport_height: f64 = 2.0;
+    let viewport_width: f64 = image_width as f64 * viewport_height / image_height as f64;
+    let camera_center: Point3 = Point3::new(0.0, 0.0, 0.0);
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     let args = Args::parse();
 
     let mut file = File::create(&args.output_file)?;
 
     // header
-    writeln!(file, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT)?;
+    writeln!(file, "P3\n{} {}\n255", image_width, image_height)?;
 
     // pixel data
-    for j in 0..IMAGE_HEIGHT {
-        println!("Scanlines remaining: {}", IMAGE_HEIGHT - j);
-        for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
+    for j in 0..image_height {
+        println!("Scanlines remaining: {}", image_height - j);
+        for i in 0..image_width {
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray = Ray::new(camera_center, pixel_center - camera_center);
+            let pixel_color = ray_color(&ray);
 
-            let pixel_color = Color::new(r, g, b);
-
-            writeln!(
-                file,
-                "{} {} {}",
-                (255.999 * pixel_color.x()) as u8,
-                (255.999 * pixel_color.y()) as u8,
-                (255.999 * pixel_color.z()) as u8
-            )?;
+            write_color(&mut file, pixel_color)?;
         }
     }
 
